3ab2306e9295d4f521c79a521a1897cde0612268
ricardo <ricardo.rsr.rodrigues@gmail.com>
2015-06-11 09:50:17 +0100

    add schema for 4E

--- src/manualE/Main.java
+++ src/manualE/Main.java
@@ -19,20 +19,17 @@ import javax.xml.validation.SchemaFactory;
 public class Main {
 
 	
-	public static void main (String[] arg){
+	public static void main (String[] arg) throws Exception{
 		
 		List<Student> students =  new ArrayList<Student>();
 		students.add(new Student("Alberto",21, 201134441110L));
 		students.add(new Student("Patricia",22,201134441116L));
 		students.add(new Student("Luis",21,201134441210L));
 		
-		try{
-			File f=new File("StudentE.xml");
+		File f=new File("StudentE.xml");
 			FileOutputStream out= new FileOutputStream(f);
 			marshalXML(out, students);
-		} catch (Exception e){
-			System.out.println(e.getMessage());
-		}
+		
 		
 		
 	}
@@ -50,8 +47,13 @@ public class Main {
 		classe.getStudents().addAll(s);
 		
 		Marshaller m = jc.createMarshaller();
-	
+		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = schemaFactory.newSchema(new File("manualE.xsd")); 
+		m.setSchema(schema);
+		
 		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
+		m.setProperty("com.sun.xml.internal.bind.xmlHeaders", 
+        	    "\n<?xml-stylesheet type=\"text/xsl\" href=\"text.xsl\" ?>");
 		m.marshal(classe, out);
 
 	}
